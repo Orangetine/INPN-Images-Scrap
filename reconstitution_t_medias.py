f3203c38d2b9c3c6a53963084ac4b2d96ce90309
Orangetine <salima.rakass@gmail.com>
2025-09-11 10:26:30 +0200

    correction

--- reconstitution_t_medias.py
+++ reconstitution_t_medias.py
@@ -5,7 +5,7 @@ import os
 IMAGES_PATH = "downloads/"
 OUTPUT_DIR = "output/"
 
-def reconstitution_t_medias_csv(chemin):
+def reconstitution_t_medias_csv(chemin = "atlas/static/custom/images/INPN_images/"):
     image_files = os.listdir(IMAGES_PATH)
     t_medias = pd.read_csv("doc/t_medias.csv", sep=";")
     filenames_t_medias = [url.split("/")[-1] for url in t_medias["url"] if pd.notna(url)]
@@ -13,12 +13,6 @@ def reconstitution_t_medias_csv(chemin):
     saved_images_nb = len(filenames_downloads)
     nb_images_initial = len(filenames_t_medias)
 
-    print(f"Nombre d'images sauvées :",
-          saved_images_nb,
-          "Nombre d'images initial",
-          nb_images_initial,
-          "Taux de sauvegarde :",
-          f"{round(saved_images_nb/nb_images_initial*100,2)}%", sep = "\n")
     t_medias["chemin"] = [
         f"{chemin}{url.split('/')[-1]}.jpg" 
         if (pd.notna(url) and url.split('/')[-1] in filenames_downloads)
@@ -27,7 +21,14 @@ def reconstitution_t_medias_csv(chemin):
     ]
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     t_medias.to_csv(f"{OUTPUT_DIR}t_medias_final.csv", sep=";", index=False)
+
+    print(f"Nombre d'images sauvées :",
+          saved_images_nb,
+          "Nombre d'images initial",
+          nb_images_initial,
+          "Taux de sauvegarde :",
+          f"{round(saved_images_nb/nb_images_initial*100,2)}%", sep = "\n")
  
 
 if __name__ == "__main__":
-    reconstitution_t_medias_csv("static/custom/INPN_images/")
+    reconstitution_t_medias_csv()
